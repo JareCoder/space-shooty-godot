ab941cda09b88d86e539878d9985aab98cf5b430
Jesse Lappalainen <jesse.m.lappalainen@gmail.com>
2026-06-11 22:44:24 +0300

    Chore: Use better function for input

--- Scenes/game_over.gd
+++ Scenes/game_over.gd
@@ -1,11 +1,10 @@
 extends Control
 
 var level_scene: PackedScene = load("res://Scenes/level.tscn")
-var scoreText: String = $CenterContainer/VBoxContainer/Score.text
 
 func _ready():
-	scoreText = scoreText + str(Global.score)
+	$CenterContainer/VBoxContainer/Score.text = $CenterContainer/VBoxContainer/Score.text + str(Global.score)
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_select"):
+func _input(event):
+	if event.is_action_pressed("ui_select"):
 		get_tree().change_scene_to_packed(level_scene)
